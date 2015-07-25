b6774846aea11c778fe21df1641c317936622c47
Aaditya Kalsi <akalsi.dev@gmail.com>
2015-07-25 19:21:46 -0400

    Add missing feature test macro

--- memory.h
+++ memory.h
@@ -33,11 +33,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <cbase/buildsym.h>
 #include <cbase/ints.h>
 
-/* STDLIB includes
- */
-#include <stdlib.h>
-#include <string.h>
-
 #if !defined(NDEBUG)
 
 CBASE_API
